4f9db6ef4e5dee7c82a6cd46bac3cb00517f1882
Anthony Miller <anthony@tiny-zebra.com>
2014-07-07 13:02:51 -0500

    Switched to PostreSQL

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
+  let(:base_title) { "Creative Soul Music School" }
 
   describe "Home page" do
 
     it "should have the content 'Creative Soul'" do
       visit '/static_pages/home'
       expect(page).to have_content('Creative Soul')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -16,6 +23,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
   end
 
   describe "About page" do
@@ -24,6 +36,23 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
+  end
 end
